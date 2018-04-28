162e9f8ad0d5649809acff9cef5956a07759be6e
Matt Burchett <matt@mattburchett.com>
2018-04-27 20:40:44 -0500

    basing off of sonarr code

--- api_radarr.rb
+++ api_radarr.rb
@@ -1,84 +1,125 @@
-def search_movie(title)
-  query = "movie.search?q=#{title}"
-  results = JSON.parse(api_query_movie(query), :symbolize_names => true)
- 
-  #movies = multiple results
-  #movie = one result
-  if ! results[:movie].nil? then
-    results[:movies] = results[:movie]
-  end
-
-  return if results[:movies] == nil
+def search_show(title)
   
-  movies = Array.new
-
-  results[:movies].each do |r|
-    if ! r[:imdb].nil?
-      movies.insert(-1, { :imdbid => r[:imdb], :title => r[:titles][0], :year => r[:year], :requested => r[:in_wanted], :downloaded => r[:in_library]})
+  def search_movie(title)
+    query = "movie.search?q=#{title}"
+    results = JSON.parse(api_query_movie(query), :symbolize_names => true)
+   
+    #movies = multiple results
+    #movie = one result
+    if ! results[:movie].nil? then
+      results[:movies] = results[:movie]
     end
+  
+    return if results[:movies] == nil
+    
+    movies = Array.new
+  
+    results[:movies].each do |r|
+      if ! r[:imdb].nil?
+        movies.insert(-1, { :imdbid => r[:imdb], :title => r[:titles][0], :year => r[:year], :requested => r[:in_wanted], :downloaded => r[:in_library]})
+      end
+    end
+  
+    return if movies.count == 0
+  
+    return movies
+  end
+  
+  def movie_imdbid_to_title(imdbid)
+    movie = search_movie("#{imdbid}")
+    return movie[0][:title]
   end
 
-  return if movies.count == 0
+def add_show()
+  #Search for this show via tvdbid to get it as the only result
+  #Sonarr needs the JSON from the result as the submission to actually download the show
+  search_results = JSON.parse(api_query("series/lookup", "term=tvdb:#{tvdbid}"))[0]
 
-  return movies
-end
+  #Copy over only the objects that Sonarr requires from the search result JSON 
+  search_json = Hash.new
+  search_json['tvdbId'] = search_results['tvdbId']
+  search_json['title'] = search_results['title']
+  search_json['qualityProfileId'] = search_results['qualityProfileId']
+  search_json['titleSlug'] = search_results['titleSlug']
+  search_json['images'] = search_results['images']
+  search_json['seasons'] = search_results['seasons']
+  search_json['rootFolderPath'] = JSON.parse(api_query("rootfolder", ""))[0]['path']
+  search_json['ProfileId'] = @sonarr_profile_id
 
-def movie_imdbid_to_title(imdbid)
-  movie = search_movie("#{imdbid}")
-  return movie[0][:title]
-end
+  #Get album art from search_results
+  album_art_url = search_results['remotePoster']
 
-def add_movie(imdbid)
-  results = api_query_movie("movie.add?identifier=#{imdbid}&profile_id=#{@cp_profile_id}")
-  j_results = JSON.parse(results, :symbolize_names => true)
-  if j_results[:success] == true
-    return true, j_results[:movie][:title], j_results[:movie][:info][:images][:poster][0], nil 
+  #POST the 'search_json.to_json' text to Sonarr to start the download
+  add_results = JSON.parse(api_query('series', '', search_json.to_json))
+
+  #success message is a hash {}. errors are an array [] of hashes {}
+  #extract the hash at index 0 of error array so that it's just a hash
+  if add_results.is_a? Array then
+    add_results = add_results[0]
+  end
+
+  if add_results['tvdbId'].to_s == tvdbid
+    return true, add_results['title'], album_art_url
   else
-    puts "Failure adding movie! IMDBID: #{imdbid}. Dumping response object..."
-    pp j_results
-    #the json failure message doesn't have the movie name (like the success json does)
-    #so we need to look it up via the imdbid. This may also fail if there's an issue with CP
-    return false, movie_imdbid_to_title(imdbid), nil, nil
+    puts "Failure adding show! tvdbID: #{tvdbid}. Dumping response object..."
+    pp add_results
+    return false, search_results['title'], nil, add_results['errorMessage']
   end
 end
 
-def search_movie_local(imdb)
-  results = JSON.parse(api_query_movie("media.get/?id=#{imdb}"), :symbolize_names => true)
+def get_show_profile_list
+  endpoint = "profile"
 
-  if ! results[:media].nil?
-    return true #movie found
-  else
-    return false #movie not found
-  end
-end
-
-def get_movie_profile_list
-  results = JSON.parse(api_query_movie("profile.list"))
+  results = JSON.parse(api_query(endpoint, ""))
   profiles = Array.new
 
-  results["list"].each {|r| profiles.insert(-1, { "name" => r["label"], "profile_id" => r["_id"]})}
+  results.each {|r| profiles.insert(-1, { "name" => r["name"], "profile_id" => r["id"]})}
 
   # [ {"1080p" => "z9y8x7w6v5u4..."}, {"720p" => "a1b2c3d4e5..."} ]
   return profiles
 end
 
-
-def api_query_movie(query)
-  url = URI.parse("#{@radarr_url}:#{@radarr_port}#{@radarr_context}/api/#{@radarr_api_key}/#{URI.escape(query)}")
-
-  request = Net::HTTP.new(url.host, url.port)
-
-  #Set timeout and scheme
-  request.use_ssl = url.scheme == 'https'
-  request.read_timeout = @radarr_timeout
-  request.open_timeout = @radarr_timeout
-
-  begin
-    #Make the request
-    response = request.start {|req| req.get(url) }
-  rescue
-    raise $!.message
+def search_show_local(imdbid)
+  JSON.parse(api_query("series", ""), :symbolize_names => true).each do |show|
+    return true if show[:tvdbId] == imdbid
   end
+  return false
+end
 
-  return response.body
+def api_query(endpoint, query, postdata = "")
+  url = URI.parse("#{@sonarr_url}:#{@sonarr_port}#{@sonarr_context}/api/#{endpoint}?#{URI.escape(query)}&apikey=#{@sonarr_api_key}")
+
+  if postdata == "" then
+    #Request being made without POST data
+    request = Net::HTTP.new(url.host, url.port)
+    request.use_ssl = url.scheme == 'https'
+    request.read_timeout = @sonarr_timeout
+    request.open_timeout = @sonarr_timeout
+  
+    begin
+      #Make the request
+      response = request.start {|req| req.get(url) }
+    rescue
+      raise $!.message
+    end
+  
+    return response.body
+  else
+    #Request being made via POST
+    request = Net::HTTP::Post.new(url)
+    request.body = postdata
+    #request.read_timeout = @sonarr_timeout
+    #request.open_timeout = @sonarr_timeout
+    
+    #Make the request    
+    begin
+      response = Net::HTTP.start(url.hostname, url.port) do |http|
+        http.request(request)
+      end
+    rescue
+      raise $!.message
+    end
+
+    return response.body
+  end
 end
